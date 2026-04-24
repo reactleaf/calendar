26cb831d15782fee2089bc990de54670ce2c368b
Hyeonseok <hyeonseok.yang@devsisters.com>
2026-04-24 13:56:31 +0900

    chore: delete removed page

--- docs/app/components/mdx.tsx
+++ docs/app/components/mdx.tsx
@@ -1,15 +1,14 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx'
-import type { MDXComponents } from 'mdx/types'
 import { DisabledDatesExample } from '@/components/doc-examples/DisabledDatesExample'
 import { LocaleWeekStartExample } from '@/components/doc-examples/LocaleWeekStartExample'
-import { MessagesFormattersExample } from '@/components/doc-examples/MessagesFormattersExample'
 import { MinMaxDatesExample } from '@/components/doc-examples/MinMaxDatesExample'
-import { MultipleModeExample } from '@/components/doc-examples/MultipleModeExample'
 import { MultipleMaxSelectionsExample } from '@/components/doc-examples/MultipleMaxSelectionsExample'
+import { MultipleModeExample } from '@/components/doc-examples/MultipleModeExample'
 import { RangeModeExample } from '@/components/doc-examples/RangeModeExample'
 import { RangeTimeExample } from '@/components/doc-examples/RangeTimeExample'
 import { SingleModeExample } from '@/components/doc-examples/SingleModeExample'
 import { SingleTimeExample } from '@/components/doc-examples/SingleTimeExample'
+import defaultMdxComponents from 'fumadocs-ui/mdx'
+import type { MDXComponents } from 'mdx/types'
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
@@ -23,7 +22,6 @@ export function getMDXComponents(components?: MDXComponents) {
     DisabledDatesExample,
     MinMaxDatesExample,
     LocaleWeekStartExample,
-    MessagesFormattersExample,
     ...components,
   } satisfies MDXComponents
 }
